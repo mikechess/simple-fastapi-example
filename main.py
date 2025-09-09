f3c35a41c98a6e91d9bfb551da153341ccf0e7f2
mikechess <mikecook450@gmail.com>
2025-09-09 17:43:50 -0400

    JSON Request and Path Parameters

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text: str 
+    is_done: bool = False
 
 items = []
 
@@ -10,13 +15,18 @@ def root():
 
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
 
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
